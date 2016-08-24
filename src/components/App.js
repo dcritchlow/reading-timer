fad038d206bce7dce468ddccb227f23d9cf16b6b
Darin Critchlow <Darin.Critchlow@selecthealth.org>
2016-08-24 17:00:52 -0600

    Randomize color of timer

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ class App extends Component {
 
     this.state = {
       seconds: 1200000,
-      color: "#000",
+      color: '#'+Math.floor(Math.random()*16777215).toString(16),
       callback: () => this.hideTimer(),
       timerVisible: false,
       buttonsDisabled: false
